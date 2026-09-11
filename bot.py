e7156a0880d120615d41f2dbda99a9058fd1fd35
PigeonHawk <lepigeonhawk@gmail.com>
2026-09-11 15:44:17 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ async def main():
         await bot.load_extension("cogs.valorant_bingo")
         await bot.load_extension("cogs.quotes")
         await bot.load_extension("cogs.contexto")
+        await bot.load_extension("cogs.birthday")
         await bot.start(TOKEN)
       
 @bot.event
